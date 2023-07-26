5604cf67a59e523b5fd4e9b6aaa29204cd37d225
Bhee100.afolabi omobolanle <afolabiomobolanle00@gmail.com>
2023-07-26 22:32:28 +0100

    test

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,57 +1,47 @@
 import React, { useState } from 'react';
 
 const Calculator = () => {
-  const [input, setInput] = useState('');
+  const [screenValue, setscreenValue] = useState('');
 
-  const handleButtonClick = (value) => {
-    setInput(input + value);
+  const handlekeyPress = (value) => {
+    setscreenValue(screenValue + value);
   };
 
-  const handleCalculate = () => {
+  const arithemeticOperations = () => {
     try {
-      setInput(eval(input).toString());
+      setscreenValue(eval(screenValue).toString());
     } catch (error) {
-      setInput('Error');
+      setscreenValue('Error');
     }
   };
 
-  const handleClear = () => {
-    setInput('');
+  const handleClearValue = () => {
+    setscreenValue('');
   };
 
   return (
-    <div className="calculator">
-      <div className="display">{input}</div>
-      <div className="buttons">
-        <div className="row">
-          <button onClick={() => handleButtonClick('7')}>7</button>
-          <button onClick={() => handleButtonClick('8')}>8</button>
-          <button onClick={() => handleButtonClick('9')}>9</button>
-          <button onClick={() => handleButtonClick('+')}>+</button>
-        </div>
-        <div className="row">
-          <button onClick={() => handleButtonClick('4')}>4</button>
-          <button onClick={() => handleButtonClick('5')}>5</button>
-          <button onClick={() => handleButtonClick('6')}>6</button>
-          <button onClick={() => handleButtonClick('-')}>-</button>
-        </div>
-        <div className="row">
-          <button onClick={() => handleButtonClick('1')}>1</button>
-          <button onClick={() => handleButtonClick('2')}>2</button>
-          <button onClick={() => handleButtonClick('3')}>3</button>
-          <button onClick={() => handleButtonClick('*')}>*</button>
-        </div>
-        <div className="row">
-          <button onClick={() => handleButtonClick('0')}>0</button>
-          <button onClick={() => handleButtonClick('.')}>.</button>
-          <button onClick={handleClear}>C</button>
-          <button onClick={() => handleButtonClick('/')}>/</button>
-        </div>
-        <div className="row">
-          <button onClick={handleCalculate}>=</button>
+    <div className="" >
+      <div className="display">{screenValue}</div>
+      <div className="">
+          <button onClick={() => handlekeyPress('7')}>7</button>
+          <button onClick={() => handlekeyPress('8')}>8</button>
+          <button onClick={() => handlekeyPress('9')}>9</button>
+          <button onClick={() => handlekeyPress('+')}>+</button>
+          <button onClick={() => handlekeyPress('4')}>4</button>
+          <button onClick={() =>handlekeyPress('5')}>5</button>
+          <button onClick={() => handlekeyPress('6')}>6</button>
+          <button onClick={() => handlekeyPress('-')}>-</button>
+          <button onClick={() => handlekeyPress('1')}>1</button>
+          <button onClick={() => handlekeyPress('2')}>2</button>
+          <button onClick={() => handlekeyPress('3')}>3</button>
+          <button onClick={() => handlekeyPress('*')}>*</button>
+          <button onClick={() => handlekeyPress('0')}>0</button>
+          <button onClick={() => handlekeyPress('.')}>.</button>
+          <button onClick={handleClearValue}>C</button>
+          <button onClick={() => handlekeyPress('/')}>/</button>
+          <button onClick={arithemeticOperations}>=</button>
         </div>
       </div>
-    </div>
   );
 };
 

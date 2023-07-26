53916735fd497827ac233f4bf1bb6f8430b2ffc3
Bhee100.afolabi omobolanle <afolabiomobolanle00@gmail.com>
2023-07-26 23:36:22 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Calculator from './Components/Calculator'
+
+
 const App = () => {
   return (
-    <div className='h-screen w-screen justify-center'>
-      <Calculator />
+    <div className=' flex justify-center items-center h-[100svh] bg-blue-100'>
+      <Calculator/>
     </div>
   )
 }
--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,48 +1,55 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+// using the functional-based Components.
 const Calculator = () => {
-  const [screenValue, setscreenValue] = useState('');
+  const [screenValue, setscreenValue] = useState("");
 
-  const handlekeyPress = (value) => {
-    setscreenValue(screenValue + value);
+  const onKeyPressHandler = (value) => {
+    setscreenValue( screenValue + value);
   };
-
-  const arithemeticOperations = () => {
+// this line clears the display.
+  const clearDisplay = () => {
+    setscreenValue("");
+  };
+// this is a try and catch error to handle possible arithemetic errors and make calculations.
+  const arithemeticOperation= () => {
     try {
       setscreenValue(eval(screenValue).toString());
     } catch (error) {
-      setscreenValue('Error');
+      setscreenValue("Error");
     }
   };
 
-  const handleClearValue = () => {
-    setscreenValue('');
-  };
-
   return (
-    <div className="" >
-      <div className="display">{screenValue}</div>
-      <div className="">
-          <button onClick={() => handlekeyPress('7')}>7</button>
-          <button onClick={() => handlekeyPress('8')}>8</button>
-          <button onClick={() => handlekeyPress('9')}>9</button>
-          <button onClick={() => handlekeyPress('+')}>+</button>
-          <button onClick={() => handlekeyPress('4')}>4</button>
-          <button onClick={() =>handlekeyPress('5')}>5</button>
-          <button onClick={() => handlekeyPress('6')}>6</button>
-          <button onClick={() => handlekeyPress('-')}>-</button>
-          <button onClick={() => handlekeyPress('1')}>1</button>
-          <button onClick={() => handlekeyPress('2')}>2</button>
-          <button onClick={() => handlekeyPress('3')}>3</button>
-          <button onClick={() => handlekeyPress('*')}>*</button>
-          <button onClick={() => handlekeyPress('0')}>0</button>
-          <button onClick={() => handlekeyPress('.')}>.</button>
-          <button onClick={handleClearValue}>C</button>
-          <button onClick={() => handlekeyPress('/')}>/</button>
-          <button onClick={arithemeticOperations}>=</button>
-        </div>
+<div className=" flex justify-center items-center h-[100svh] w-20">
+  <div className=" p-9 h-[70%] bg-purple-500 rounded-md 2rem;">
+     <div className=" flex justify-center items-center mt-8">
+     <input type="text" value={screenValue} readOnly className=" w-[100%] h-12 flex items-center mb-6 " />
+     </div>
+      <div className=" grid grid-cols-[1fr_1fr_1fr_1fr] gap-2 font-black text-2xl">
+        <button onClick={() => clearDisplay()} className=" w-20 h-10 border ">C</button>
+        <button onClick={() => onKeyPressHandler("0")} className=" w-20 h-10 border ">0</button>
+        <button onClick={() => onKeyPressHandler("1")} className=" w-20 h-10 border ">1</button>
+        <button onClick={() => onKeyPressHandler("2")} className=" w-20 h-10 border ">2</button>
+        <button onClick={() => onKeyPressHandler("3")} className=" w-20 h-10 border ">3</button>
+        <button onClick={() => onKeyPressHandler("4")} className=" w-20 h-10 border ">4</button>
+        <button onClick={() => onKeyPressHandler("5")} className=" w-20 h-10 border ">5</button>
+        <button onClick={() => onKeyPressHandler("7")} className=" w-20 h-10 border ">7</button>
+        <button onClick={() => onKeyPressHandler("6")} className=" w-20 h-10 border ">6</button>
+        <button onClick={() => onKeyPressHandler("8")} className=" w-20 h-10 border ">8</button>
+        <button onClick={() => onKeyPressHandler("9")} className=" w-20 h-10 border ">9</button>
+        <button onClick={() => onKeyPressHandler("-")} className=" w-20 h-10 border ">-</button>
+        <button onClick={() => onKeyPressHandler("+")} className=" w-20 h-10 border ">+</button>
+        <button onClick={() => onKeyPressHandler("*")} className=" w-20 h-10 border ">*</button>
+        <button onClick={() => onKeyPressHandler("/")} className=" w-20 h-10 border ">/</button>
+        <button onClick={() => onKeyPressHandler(".")} className=" w-20 h-10 border ">.</button>
+        <button onClick={() => arithemeticOperation()} className=" w-20 h-10 border ">=</button>
       </div>
+    </div>
+</div>
   );
 };
 
-export default Calculator;
+
+  
+
+export default Calculator
